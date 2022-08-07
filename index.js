87e388ec7b09e5328962cb8c7f262886dc109b58
ReshiWiz <nanthamjevz123@gmail.com>
2022-08-07 19:56:21 +0530

    second
    second time adding

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//----------------------------Task-1------------------------------------------------------------/
+    // 1 => Display message as 'Welcome to JavaScript' using alert(),console.log,and document.write()
+
+    // alert('Welcome to JavaScript')
+    // console.log('Welcome to JavaScript');
+    // document.write('Welcome to JavaScript')
+//-------------------------------Task-2------------------------------------------------------------//
+
+    //2 => Show alert as 'This os from external file JS file.
+// alert('This is external JavaScript')
+
+//-------------------------------Task-3------------------------------------------------------------//
+   // 3=> declare tje following variables 
+// let  variables =  {
+//     // Adding the variable
+//     userId : 'C2S0411RY',
+//     firstName : 'Jeeva',
+//     lastName : 'wiz',
+//     password : 11111111,
+//     email : 'wiz@74.yahoo.com',
+//     gender : 'Male',
+//     age : 22,
+// };
+//     // Assigning the variables 
+// const accountCreated = variables
+//     //Displaying the variables in console,window,alert
+
+//    // outPut =>
+// // const accountCreated = Object.entries(variables);
+// console.log(accountCreated);
+// alert(variables)
+// document.write(accountCreated ) 
+
+//-------------------------------Task-4------------------------------------------------------------//
+//  4=> create the variables for storing the product details with appropriate variable names and display the result in DOM
+
+// 

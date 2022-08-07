eb36ee7dd144b674503bad41dc72252c8390aac6
ReshiWiz <nanthamjevz123@gmail.com>
2022-08-07 20:12:49 +0530

    4th over

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
 //----------------------------Task-1------------------------------------------------------------/
-    // 1 => Display message as 'Welcome to JavaScript' using alert(),console.log,and document.write()
+// 1 => Display message as 'Welcome to JavaScript' using alert(),console.log,and document.write()
 
-    // alert('Welcome to JavaScript')
-    // console.log('Welcome to JavaScript');
-    // document.write('Welcome to JavaScript')
+// alert('Welcome to JavaScript')
+// console.log('Welcome to JavaScript');
+// document.write('Welcome to JavaScript')
 //-------------------------------Task-2------------------------------------------------------------//
 
-    //2 => Show alert as 'This os from external file JS file.
+//2 => Show alert as 'This os from external file JS file.
 // alert('This is external JavaScript')
 
 //-------------------------------Task-3------------------------------------------------------------//
-   // 3=> declare tje following variables 
+// 3=> declare tje following variables
 // let  variables =  {
 //     // Adding the variable
 //     userId : 'C2S0411RY',
@@ -22,7 +21,7 @@
 //     gender : 'Male',
 //     age : 22,
 // };
-//     // Assigning the variables 
+//     // Assigning the variables
 // const accountCreated = variables
 //     //Displaying the variables in console,window,alert
 
@@ -30,7 +29,7 @@
 // // const accountCreated = Object.entries(variables);
 // console.log(accountCreated);
 // alert(variables)
-// document.write(accountCreated ) 
+// document.write(accountCreated )
 
 //-------------------------------Task-4------------------------------------------------------------//
 //  4=> create the variables for storing the product details with appropriate variable names and display the result in DOM
